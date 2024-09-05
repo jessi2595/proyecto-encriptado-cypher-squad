2eb8999fea0dcf569cfaecea0522c780df0352ed
SaulSneider7 <saulsneider@hotmail.com>
2024-09-04 19:05:51 -0500

    correccion al vincular el JS en formulario

--- assets/formulario.js
+++ assets/formulario.js
@@ -1,22 +1,22 @@
 
 function pintarNombre() {
-    document.getElementById("saludo").innerHTML += localStorage.getItem("name"); 
+    document.getElementById("saludo").innerHTML += localStorage.getItem("name");
 }
 pintarNombre()
 
-function genero(){
+function genero() {
     const form = document.getElementById('access-from');
-    
-    
-        form.addEventListener('submit',(e) =>{
+
+
+    form.addEventListener('submit', (e) => {
         e.preventDefault();
         const gender = document.querySelector('input[name="gender"]:checked').value;
         if (gender === 'female') {
             window.location.href = 'inicio.html';
         } else if (gender === 'male') {
             window.location.href = 'men-only.html';
-            
+
         }
-    }); 
-    }
-    genero()
+    });
+}
+genero()
